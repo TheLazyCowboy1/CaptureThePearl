e74a573630b91888e5e7ca412b5456ac383830a8
TheLazyCowboy1 <Nathan@331oak.com>
2025-03-09 13:32:28 -0400

    Working client backgrounds

--- CTPMenu.cs
+++ CTPMenu.cs
@@ -30,13 +30,18 @@ public class CTPMenu : StoryOnlineMenu
         //add region dropdowns
         SetupRegionDropdown();
 
-        ChangePageBackground();
+        //remove "match save" option
+        if (clientWantsToOverwriteSave != null) clientWantsToOverwriteSave.Checked = false;
+        clientWantsToOverwriteSave?.RemoveSprites();
+
+        //ChangePageBackground();
+        AdjustBackgroundLocation();
 
         previousPageIdx = slugcatPageIndex;
     }
 
     private int previousPageIdx;
-    private string previousRegion = "SU";
+    private string previousRegion = "";
     public override void Update()
     {
         base.Update();
@@ -44,7 +49,7 @@ public class CTPMenu : StoryOnlineMenu
         if (OnlineManager.lobby.isOwner) //host update stuff
         {
             //RegionSelected = RegionDropdownBox.value;
-            storyGameMode.region = Region.GetRegionFullName(RegionDropdownBox.value, slugcatPages[slugcatPageIndex].slugcatNumber);
+            storyGameMode.region = RegionDropdownBox.value;
 
             if (slugcatPageIndex != previousPageIdx)
             {
@@ -60,7 +65,11 @@ public class CTPMenu : StoryOnlineMenu
             if (storyGameMode.region != previousRegion)
             {
                 ChangePageBackground();
-                previousRegion = storyGameMode.region;
+                //previousRegion = storyGameMode.region; //changed in ChangePageBackground() instead
+            }
+            if (onlineDifficultyLabel != null)
+            {
+                onlineDifficultyLabel.text = GetCurrentCampaignName() + (string.IsNullOrEmpty(storyGameMode.region) ? Translate(" - Unknown Region") : " - " + Translate(Region.GetRegionFullName(storyGameMode.region, storyGameMode.currentCampaign)));
             }
         }
     }
@@ -102,8 +111,58 @@ public class CTPMenu : StoryOnlineMenu
         if (string.IsNullOrEmpty(storyGameMode.region)) return; //don't process false regions
         try
         {
-            slugcatPages[slugcatPageIndex].slugcatImage?.RemoveSprites();
-            slugcatPages[slugcatPageIndex].slugcatImage = new InteractiveMenuScene(this, slugcatPages[slugcatPageIndex], Region.GetRegionLandscapeScene(storyGameMode.region));
+            //find original image
+            //int slugImageIdx = slugcatPages[slugcatPageIndex].subObjects.IndexOf(slugcatPages[slugcatPageIndex].slugcatImage);
+            //if (slugcatPageIndex < 0) return; //if the background doesn't exist yet... don't change it, I guess
+
+            //remove sprites
+            if (slugcatPages[slugcatPageIndex].slugcatImage != null)
+            {
+                //pages[0].RemoveSubObject(slugcatPages[slugcatPageIndex].slugcatImage);
+                //slugcatPages[slugcatPageIndex].RemoveSubObject(slugcatPages[slugcatPageIndex].slugcatImage);
+                slugcatPages[slugcatPageIndex].slugcatImage.UnloadImages();
+                slugcatPages[slugcatPageIndex].slugcatImage.RemoveSprites();
+                //slugcatPages[slugcatPageIndex].slugcatImage = null;
+                //slugcatPages[slugcatPageIndex].subObjects[slugcatPageIndex]?.RemoveSprites();
+
+                /*slugcatPages[slugcatPageIndex].slugcatImage.sceneID = Region.GetRegionLandscapeScene(storyGameMode.region);
+                slugcatPages[slugcatPageIndex].slugcatImage.BuildScene();
+                AddBackgroundIllustrations(slugcatPages[slugcatPageIndex].slugcatImage);*/
+                slugcatPages[slugcatPageIndex].slugcatImage = new InteractiveMenuScene(this, slugcatPages[slugcatPageIndex], Region.GetRegionLandscapeScene(storyGameMode.region));
+
+                slugcatPages[slugcatPageIndex].slugcatImage.Show();
+                slugcatPages[slugcatPageIndex].slugcatImage.Container.MoveToBack(); //don't let it cover up anything else; that's annoying
+
+                previousRegion = storyGameMode.region;
+
+                RainMeadow.RainMeadow.Debug($"[CTP]: Changed background region scene to {previousRegion}.");
+            }
+
+            //create new background
+            //new MenuScene()
+            //slugcatPages[slugcatPageIndex].slugcatImage = new InteractiveMenuScene(this, pages[0], Region.GetRegionLandscapeScene(storyGameMode.region));
+
+            //replace old background
+            //slugcatPages[slugcatPageIndex].subObjects[slugcatPageIndex] = slugcatPages[slugcatPageIndex].slugcatImage;
+            //slugcatPages[slugcatPageIndex].subObjects.Insert(slugImageIdx, slugcatPages[slugcatPageIndex].slugcatImage);
+            //pages[0].subObjects.Add(slugcatPages[slugcatPageIndex].slugcatImage);
+            //slugcatPages[slugcatPageIndex].slugcatImage.
+            //slugcatPages[slugcatPageIndex].slugcatImage.Show();
+
+            //previousRegion = storyGameMode.region;
         } catch { }
+    }
+
+    private void AdjustBackgroundLocation()
+    {
+        if (OnlineManager.lobby.isOwner) return; //don't change host's background
+        if (slugcatPages[slugcatPageIndex].slugcatImage == null)
+        {
+            RainMeadow.RainMeadow.Debug("[CTP]: Couldn't change background image; no background to be changed!!");
+            return;
+        }
+        slugcatPages[slugcatPageIndex].slugcatImage.Container.alpha = 0.7f; //make it partially transparent
+        //slugcatPages[slugcatPageIndex].slugcatImage.Container.SetPosition(0, 0); //center it
+        slugcatPages[slugcatPageIndex].slugcatImage.Container.scale = 0.5f; //increase scale so it doesn't cover the entire screen
     }
 }
