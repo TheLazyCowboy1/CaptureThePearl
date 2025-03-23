6457cd440a55a0c386ff9f2ed4bd71afc5a49809
Pocky-raisin <160378936+Pocky-raisin@users.noreply.github.com>
2025-03-23 20:10:19 +0000

    Removed host button because it broken

--- src/CTPMenuHooks.cs
+++ src/CTPMenuHooks.cs
@@ -16,8 +16,6 @@ namespace CaptureThePearl
         public static void RemoveHooks()
         {
             On.ProcessManager.PostSwitchMainProcess -= ProcessManager_PostSwitchMainProcess;
-            On.Menu.SlugcatSelectMenu.AddColorButtons -= SlugcatSelectMenu_AddColorButtons;
-            On.Menu.SimpleButton.Clicked -= SimpleButton_Clicked;
             On.Menu.SlugcatSelectMenu.StartGame -= SlugcatSelectMenu_StartGame;
 
             On.PlayerProgression.IsThereASavedGame -= PlayerProgression_IsThereASavedGame;
@@ -27,8 +25,6 @@ namespace CaptureThePearl
         public static void ApplyHooks()
         {
             On.ProcessManager.PostSwitchMainProcess += ProcessManager_PostSwitchMainProcess;
-            On.Menu.SlugcatSelectMenu.AddColorButtons += SlugcatSelectMenu_AddColorButtons;
-            On.Menu.SimpleButton.Clicked += SimpleButton_Clicked;
             On.Menu.SlugcatSelectMenu.StartGame += SlugcatSelectMenu_StartGame;
 
             On.PlayerProgression.IsThereASavedGame += PlayerProgression_IsThereASavedGame;
@@ -52,38 +48,6 @@ namespace CaptureThePearl
             if (self is CTPMenu menu) menu.StartGame(storyGameCharacter);
             else orig(self, storyGameCharacter);
         }
-
-        private static void SlugcatSelectMenu_AddColorButtons(On.Menu.SlugcatSelectMenu.orig_AddColorButtons orig, SlugcatSelectMenu self)
-        {
-            if (self is CTPMenu storyMenu && OnlineManager.lobby.isOwner)
-            {
-                if (self.colorInterface == null)
-                {
-                    Vector2 pos = new Vector2(1000f - (1366f - self.manager.rainWorld.options.ScreenSize.x) / 2f, self.manager.rainWorld.options.ScreenSize.y - 100f);
-                    self.colorInterface = self.GetColorInterfaceForSlugcat(storyMenu.slugcatColorOrder[storyMenu.hostSlugIndex], pos);
-                    self.pages[0].subObjects.Add(self.colorInterface);
-                }
-            }
-            else orig(self);
-        }
-        private static void SimpleButton_Clicked(On.Menu.SimpleButton.orig_Clicked orig, SimpleButton self)
-        {
-            orig(self);
-            if (self.signalText != null && self.signalText == "CTPHostScugButton" && self.menu != null && self.menu is CTPMenu storyMenu)
-            {
-                storyMenu.hostSlugIndex = (storyMenu.hostSlugIndex > storyMenu.slugcatColorOrder.Count - 1) ? 0 : storyMenu.hostSlugIndex + 1;
-                storyMenu.personaSettings.playingAs = storyMenu.slugcatColorOrder[storyMenu.hostSlugIndex];
-                storyMenu.storyGameMode.avatarSettings.playingAs = storyMenu.slugcatColorOrder[storyMenu.hostSlugIndex];
-                if (storyMenu.colorChecked)
-                {
-                    storyMenu.RemoveColorButtons();
-                    storyMenu.AddColorButtons();
-                }
-
-                storyMenu.hostScugButton.menuLabel.text = SlugcatStats.getSlugcatName(storyMenu.slugcatColorOrder[storyMenu.hostSlugIndex]);
-            }
-        }
-
         private static void ProcessManager_PostSwitchMainProcess(On.ProcessManager.orig_PostSwitchMainProcess orig, ProcessManager self, ProcessManager.ProcessID ID)
         {
             if (ID == Plugin.CTPMenuProcessID) self.currentMainLoop = new CTPMenu(self);
